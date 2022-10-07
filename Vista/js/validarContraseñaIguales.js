bcf238f600b0e0d9f645a4ef4c8351e2eabc94e7
Sebastian <sebastian.legnazzi@est.fi.uncoma.edu.ar>
2022-10-07 19:05:25 -0300

    Modifico para que no se vea el tilde verde cuando da error

--- Vista/js/validarContraseñaIguales.js
+++ Vista/js/validarContraseñaIguales.js
@@ -3,8 +3,8 @@ function verificarContraseñaIgual(contraseña, confContraseña){
     let divCorrecto = document.getElementsByClassName("password-correcta")
     let divInvalidPass = document.getElementsByClassName("invalid-password")
     if((contraseña.value != confContraseña.value)){
-        contraseña.style="border: 1px solid red; color: red;";
-        confContraseña.style="border: 1px solid red; color: red;";
+        contraseña.style="border: 1px solid red; color: red; background-image: none;";
+        confContraseña.style="border: 1px solid red; color: red; background-image: none;";
         divCorrecto[0].style="display: none;"
         divCorrecto[1].style="display: none;"
         divInvalidPass[0].style="display: block; color: red;"
