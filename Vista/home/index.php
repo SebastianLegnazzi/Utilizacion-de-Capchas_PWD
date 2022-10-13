520afffdeac0aa38e1a30a497483d972002f5775
Sebastian <sebileg@hotmail.com>
2022-10-13 20:43:57 -0300

    Inicio el index de explicacion

--- Vista/home/index.php
+++ Vista/home/index.php
@@ -1,6 +1,34 @@
 <?php
 include_once("../estructura/Cabecera.php")
 ?>
+<div class="container-fluid pe-5 ps-5 text-center text-light">
+    <div id="que-es">
+        <div class="display-4 text-warning">Que es un captcha?</div>
+        <div class="lead text-start fs-6 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quisquam vel odio obcaecati deserunt ut consequatur doloremque qui placeat repellat pariatur fugiat saepe, ex, dolores illo sunt praesentium ipsum eum.</div>
+    </div>
+    <hr>
+    <div id="como-funciona">
+        <div class="display-4 text-warning">Como funciona?</div>
+        <div class="lead text-start fs-6 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quisquam vel odio obcaecati deserunt ut consequatur doloremque qui placeat repellat pariatur fugiat saepe, ex, dolores illo sunt praesentium ipsum eum.</div>
+    </div>
+    <hr>
+    <div id="tipos">
+        <div class="display-4 text-warning">Tipos de Captchas</div>
+        <div class="lead text-start fs-6 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quisquam vel odio obcaecati deserunt ut consequatur doloremque qui placeat repellat pariatur fugiat saepe, ex, dolores illo sunt praesentium ipsum eum.</div>
+    </div>
+    <hr>
+    <div id="comparacion">
+        <div class="display-4 text-warning">Comparacion</div>
+        <div class="row text-center">
+            <div class="lead text-start fs-6 col-md-4 mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem dolore cum dolorem totam eos excepturi consequuntur, repudiandae dolores beatae quod tempora illo voluptate iusto. Maxime ratione quibusdam temporibus dolores.
+            </div>
+            <div class="lead text-start fs-6 col-md-4 mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem dolore cum dolorem totam eos excepturi consequuntur, repudiandae dolores beatae quod tempora illo voluptate iusto. Maxime ratione quibusdam temporibus dolores.
+            </div>
+            <div class="lead text-start fs-6 col-md-4 mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem dolore cum dolorem totam eos excepturi consequuntur, repudiandae dolores beatae quod tempora illo voluptate iusto. Maxime ratione quibusdam temporibus dolores.
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 include_once("../estructura/Pie.php")
 ?>
