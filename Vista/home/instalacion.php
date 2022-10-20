3ccca00de62da8bacbf35a61f5e6f0615ba7a0df
FranciscoBarboza <francisco.barboza.tomas@gmail.com>
2022-10-19 22:06:47 -0300

    agrego las imagenes de instalacion(falta arreglar tamaños)

--- Vista/home/instalacion.php
+++ Vista/home/instalacion.php
@@ -13,30 +13,27 @@ include_once("../estructura/Cabecera.php")
                 Aaqui debemos crear una cuenta con nuestro email, contraseña, y nombre de dominio.
                 una vez creada no data la &lt;SiteKey&gt;, y la &lt;TestKey&gt; ademas devemos agregar a "Development & Test Env Domains" el dominio localhost para poder asi ver el captcha sin tener un dominio comprado.
                 en link <a class="link-warning" href="https://service.mtcaptcha.com/mtcv1/demo/">"get embed code"</a>
-                <br><br>
-                (imagen del login de mt captcha)
             </p>
+            <img src="../img/mtCaptcha_instalacion/login_mtcapthca.png" alt="">
             <p>
                 Una vez tengamos la cuenta y la &lt;SiteKey&gt; podremos acceder al <a class="link-warning" href="https://service.mtcaptcha.com/mtcv1/demo/">GENERADOR DE CODIGO</a> de MTCaptcha donde completaremos con el &lt;TestKey&gt;
                 y nombre del dominio.
-                <br>
-                <br>
-                (imagen del generador)
-                <br>
-                <br>
+            </p>
+            
+                
+            <img src="../img/mtCaptcha_instalacion/generador_codigo_mtcaptcha.png" alt="">
+                
+            <p>
                 Aca podremos agregar nuestra &lt;SiteKey&gt; ademas personalizar lenguaje, estilos, temas, y tambien activar el "Auto Form Validate" que permite que solo se podra hacer Submit del formulario si completo correctamente el MtCaptcha.
             </p>
             <p>
                 una vez seleccionado personalizado como queremos nuestro captcha le damos a "aplicar" para asi poder ver una previa de como se vera el captcha en nuestra pagina.
-                <br><br>
-                (imagen de la vista previa)
-
             </p>
+            <img src="../img/mtCaptcha_instalacion/vista_previa.png" alt="">
             <p>
                 Esto nos devolvera el codigo javascript para agregarlo a nuestra pagina.
-                <br><br>
-                (imagen del codigo embebido)
             </p>
+            <img src="../img/mtCaptcha_instalacion/imagen_codigoEmbed.png" alt="">
             <p>
                 Ahora solo tenemos que agregarlo a nuestro codigo y el div del captcha dentro del formulario que queramos proteger.
                 y ya estaria listo nuestro captcha listo para usarse.
