749d51685d9914e9874481196c116a1ad4fa1d7e
Sebastian <sebastian.legnazzi@est.fi.uncoma.edu.ar>
2022-10-21 13:08:00 -0300

    Elimino ejemplo mostrado en clase

--- ejemploCapcha/pruebaReCapchav2GET.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$respuestaServidor = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lf95XwiAAAAAI4_sQDrVsPEH2U6pSEtww5V9UZ1&response=".$_GET['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']);
-$arrayResultado = json_decode($respuestaServidor, TRUE);
-print_r($arrayResultado);
-
-
-?>
--- ejemploCapcha/pruebaReCapchav3GET.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$respuestaServidor = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfmtoIiAAAAAB1gwrwaEaRPr4isiIXvR-UlEr6B&response=".$_GET['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']);
-$arrayResultado = json_decode($respuestaServidor, TRUE);
-print_r($arrayResultado);
-
-?>
